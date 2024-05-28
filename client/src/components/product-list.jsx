6fcb06d67ec9608f160055a3f46f9a860de0114b
nishanthan <nishanthan@codevus.com>
2024-05-28 21:51:44 +0530

    getting the first output

---
+++ client/src/components/product-list.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function productlist() {
+  return (
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center '>
+
+    </div>
+  )
+}
+
+export default productlist;

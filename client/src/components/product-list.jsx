99d9a3164baaf42c52b17ae8022eccb63dcdf878
nishanthan <nishanthan@codevus.com>
2024-05-28 21:56:17 +0530

    project cleaning done

--- client/src/components/product-list.jsx
+++ client/src/components/product-list.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function productlist() {
   return (
-    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center '>
+    <div className=''>
 
     </div>
   )
